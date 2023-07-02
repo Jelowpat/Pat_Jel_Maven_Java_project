74130c507af6c6c24fde12444cfd3a5a786907e8
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-03 00:43:08 +0200

    changed assertions in test file

--- src/test/java/lessonSixCalculator/CalculatorTest.java
+++ src/test/java/lessonSixCalculator/CalculatorTest.java
@@ -7,29 +7,21 @@ class CalculatorTest {
 
     @Test
     public void shouldAddTwoNumbers(){
-        Assertions.assertEquals(3, Calculator.addTwoNumbers(2, 1));
-        Assertions.assertEquals(11, Calculator.addTwoNumbers(7, 4));
         Assertions.assertEquals(15635, Calculator.addTwoNumbers(12313, 3322));
     }
 
     @Test
     public void shouldSubtractNumberFromAnother(){
-        Assertions.assertEquals(1, Calculator.subtractNumberFromAnother(2, 1));
-        Assertions.assertEquals(3, Calculator.subtractNumberFromAnother(7, 4));
         Assertions.assertEquals(8991, Calculator.subtractNumberFromAnother(12313, 3322));
     }
 
     @Test
     public void shouldMultiplyTwoNumbers(){
-        Assertions.assertEquals(2, Calculator.multiplyTwoNumbers(2, 1));
-        Assertions.assertEquals(28, Calculator.multiplyTwoNumbers(7, 4));
         Assertions.assertEquals(40903786, Calculator.multiplyTwoNumbers(12313, 3322));
     }
 
     @Test
     public void shouldDivideNumberByAnother(){
-        Assertions.assertEquals(2, Calculator.divideNumberByAnother(2, 1));
-        Assertions.assertEquals(7D/4D, Calculator.divideNumberByAnother(7, 4));
         Assertions.assertEquals(12313D/3322D, Calculator.divideNumberByAnother(12313, 3322));
     }
     @Test
