bc8b47816f175f1e653a306faca12b7d62f858db
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-03 00:29:41 +0200

    changed package's names

--- src/main/java/lessonSixCalculator/Calculator.java
+++ src/main/java/lessonSixCalculator/Calculator.java
@@ -1,4 +1,4 @@
-package LessonSixCalculator;
+package lessonSixCalculator;
 
 public class Calculator {
 
--- src/test/java/lessonSixCalculator/CalculatorTest.java
+++ src/test/java/lessonSixCalculator/CalculatorTest.java
@@ -1,4 +1,4 @@
-package LessonSixCalculator;
+package lessonSixCalculator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
