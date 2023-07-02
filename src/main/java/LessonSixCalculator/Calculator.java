3c8b146c79a028304f2ecf789fdb1eb49d8957dc
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-02 23:21:38 +0200

    added calculator instructions tests

---
+++ src/main/java/LessonSixCalculator/Calculator.java
@@ -0,0 +1,20 @@
+package LessonSixCalculator;
+
+public class Calculator {
+
+    public static int addTwoNumbers(int a, int b){
+        return a + b;
+    }
+
+    public static int subtractANumberFromAnother(int a, int b){
+        return a - b;
+    }
+
+    public static long multiplyTwoNumbers(int a, int b){
+        return (long) a * (long) b;
+    }
+
+    public static double divideNumberByAnother(int a, int b){
+        return (double) a / (double) b;
+    }
+}
---
+++ src/test/java/LessonSixCalculator/CalculatorTest.java
@@ -0,0 +1,35 @@
+package LessonSixCalculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    public void addTwoNumbersTest(){
+        Assertions.assertEquals(3, Calculator.addTwoNumbers(2, 1));
+        Assertions.assertEquals(11, Calculator.addTwoNumbers(7, 4));
+        Assertions.assertEquals(15635, Calculator.addTwoNumbers(12313, 3322));
+    }
+
+    @Test
+    public void subtractANumberFromAnotherTest(){
+        Assertions.assertEquals(1, Calculator.subtractANumberFromAnother(2, 1));
+        Assertions.assertEquals(3, Calculator.subtractANumberFromAnother(7, 4));
+        Assertions.assertEquals(8991, Calculator.subtractANumberFromAnother(12313, 3322));
+    }
+
+    @Test
+    public void multiplyTwoNumbersTest(){
+        Assertions.assertEquals(2, Calculator.multiplyTwoNumbers(2, 1));
+        Assertions.assertEquals(28, Calculator.multiplyTwoNumbers(7, 4));
+        Assertions.assertEquals(40903786, Calculator.multiplyTwoNumbers(12313, 3322));
+    }
+
+    @Test
+    public void divideNumberByAnotherTest(){
+        Assertions.assertEquals(2, Calculator.divideNumberByAnother(2, 1));
+        Assertions.assertEquals(7D/4D, Calculator.divideNumberByAnother(7, 4));
+        Assertions.assertEquals(12313D/3322D, Calculator.divideNumberByAnother(12313, 3322));
+    }
+}
