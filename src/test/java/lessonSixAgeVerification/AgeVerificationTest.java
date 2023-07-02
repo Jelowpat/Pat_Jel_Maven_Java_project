674ecdb0665bbbdeb83b89750b4abf55401eb9e6
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-03 00:35:47 +0200

    changed test method names and improved test

--- src/test/java/lessonSixAgeVerification/AgeVerificationTest.java
+++ src/test/java/lessonSixAgeVerification/AgeVerificationTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 class AgeVerificationTest {
 
     @Test
-    public void verifyingAgeTest() {
+    public void shouldBeVerified() {
         Assertions.assertTrue(AgeVerification.verifyingAge(18));
         Assertions.assertTrue(AgeVerification.verifyingAge(35));
+    }
+    @Test
+    public void shouldBeNotVerified() {
         Assertions.assertFalse(AgeVerification.verifyingAge(17));
         Assertions.assertFalse(AgeVerification.verifyingAge(-11));
     }
