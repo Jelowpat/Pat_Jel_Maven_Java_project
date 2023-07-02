c2a8c1536f751ccc69ce6edba7c984b56478c951
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-03 00:37:04 +0200

    changed test method names and improved tests

--- src/test/java/lessonSixOddOrEven/OddOrEvenTest.java
+++ src/test/java/lessonSixOddOrEven/OddOrEvenTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 class OddOrEvenTest {
 
     @Test
-    public void checkIfEvenTest() {
+    public void shouldBeEven() {
         Assertions.assertEquals("even", OddOrEven.CheckIfEvenOrOdd(2));
-        Assertions.assertNotEquals("even", OddOrEven.CheckIfEvenOrOdd(3));
     }
     @Test
-    public void checkIfOddTest() {
-        Assertions.assertEquals("odd", OddOrEven.CheckIfEvenOrOdd(1));
-        Assertions.assertNotEquals("odd", OddOrEven.CheckIfEvenOrOdd(12));
+    public void shouldBeOdd() {
+        Assertions.assertEquals("odd", OddOrEven.CheckIfEvenOrOdd(1111));
     }
 }
