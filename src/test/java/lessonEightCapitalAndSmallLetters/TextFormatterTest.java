8fcce43189749beb273b37da51fce354b3d9d9e1
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-12 14:38:23 +0200

    applied small changes

--- src/test/java/lessonEightCapitalAndSmallLetters/TextFormatterTest.java
+++ src/test/java/lessonEightCapitalAndSmallLetters/TextFormatterTest.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TextFormatterTest {
-
-    TextToUpper text1 = new TextToUpper();
-    TextToLower text2 = new TextToLower();
-
     @Test
-    void shouldFormatTextToUpper() {
-        Assertions.assertEquals( "QWERT", text1.formatText("qWeRt"));
+    public void shouldFormatTextToUpper() {
+        TextToUpper text1 = new TextToUpper();
+        Assertions.assertEquals("QWERT", text1.formatText("qWeRt"));
     }
     @Test
-    void shouldFormatTextToULower() {
-        Assertions.assertEquals( "qwert", text2.formatText("qWeRt"));
+    public void shouldFormatTextToLower() {
+        TextToLower text2 = new TextToLower();
+        Assertions.assertEquals("qwert", text2.formatText("qWeRt"));
     }
 
 }
