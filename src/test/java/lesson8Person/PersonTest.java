0237b2b297ab556d6567b3d316e9943d64e73895
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-14 16:49:34 +0200

    applied changes to objects in tests

--- src/test/java/lesson8Person/PersonTest.java
+++ src/test/java/lesson8Person/PersonTest.java
@@ -4,39 +4,51 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PersonTest {
-
-    private Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
-            180, 74);
-    private Person person2 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
-            180, 74);
-    private Person person3 = new Person("Jacek", "Nowak", 1994, Month.December, 5,
-            180, 74);
-
     @Test
     public void shouldBeTheSameHashCode() {
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
+        Person person2 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertEquals(person1.hashCode(), person2.hashCode());
     }
     @Test
     public void shouldBeDifferentHashCode() {
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
+        Person person3 = new Person("Jacek", "Nowak", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertNotEquals(person1.hashCode(), person3.hashCode());
     }
 
     @Test
     public void shouldEqual() {
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
+        Person person2 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertEquals(person1, person2);
     }
     @Test
     public void shouldNotEqual(){
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
+        Person person3 = new Person("Jacek", "Nowak", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertNotEquals(person1, person3);
     }
 
     @Test
     public void shouldGetAge() {
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertEquals( 28, person1.getAge());
     }
 
     @Test
     public void shouldDisplayCorrectStringPattern() {
+        Person person1 = new Person("Patryk", "Jelowicki", 1994, Month.December, 5,
+                180, 74);
         Assertions.assertEquals("Person{name=Patryk, surname=Jelowicki, age=28" +
                 ", height=180.0, weight=74.0, dateOfBirth=1994-12-05}", person1.toString());
     }
