ed9a933749b88494135ea5a06f2e4754e478b9b9
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-14 16:19:52 +0200

    added more tests

--- src/test/java/lesson8WeekDaysNames/DayOfTheWeekTest.java
+++ src/test/java/lesson8WeekDaysNames/DayOfTheWeekTest.java
@@ -4,9 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DayOfTheWeekTest {
-
     @Test
-    public void shouldReturnNameOfTheDay() {
-        Assertions.assertEquals("Monday", DayOfTheWeek.whatDayIsIt(WeekDay.Monday));
+    public void shouldBeMonday() {
+        Assertions.assertEquals("Monday", DayOfTheWeek.getDayName(WeekDay.Monday));
+    }
+    @Test
+    public void shouldBeTuesday() {
+        Assertions.assertEquals("Tuesday", DayOfTheWeek.getDayName(WeekDay.Tuesday));
+    }
+    @Test
+    public void shouldBeWednesday() {
+        Assertions.assertEquals("Wednesday", DayOfTheWeek.getDayName(WeekDay.Wednesday));
+    }
+    @Test
+    public void shouldBeThursday() {
+        Assertions.assertEquals("Thursday", DayOfTheWeek.getDayName(WeekDay.Thursday));
+    }
+    @Test
+    public void shouldBeFriday() {
+        Assertions.assertEquals("Friday", DayOfTheWeek.getDayName(WeekDay.Friday));
+    }
+    @Test
+    public void shouldBeSaturday() {
+        Assertions.assertEquals("Saturday", DayOfTheWeek.getDayName(WeekDay.Saturday));
+    }
+    @Test
+    public void shouldBeSunday() {
+        Assertions.assertEquals("Sunday", DayOfTheWeek.getDayName(WeekDay.Sunday));
     }
 }
