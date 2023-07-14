9af9d027777e5a4429858668f4c58b8a0c744258
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-14 16:14:06 +0200

    deleted values of days

--- src/main/java/lesson8WeekDaysNames/WeekDay.java
+++ src/main/java/lesson8WeekDaysNames/WeekDay.java
@@ -1,21 +1,11 @@
 package lesson8WeekDaysNames;
 
 public enum WeekDay {
-    Monday(1),
-    Tuesday(2),
-    Wednesday(3),
-    Thursday(4),
-    Friday(5),
-    Saturday(6),
-    Sunday(7);
-
-    private final int DayNumber;
-
-    WeekDay(int DayNumber) {
-        this.DayNumber = DayNumber;
-    }
-
-    public int getDayNumber(){
-        return DayNumber;
-    }
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday;
 }
