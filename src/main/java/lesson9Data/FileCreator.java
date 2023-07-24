484dce8f7da54cd67552bae3b02f2d9596a03666
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-24 22:52:36 +0200

    created file data.txt with its content

---
+++ src/main/java/lesson9Data/FileCreator.java
@@ -0,0 +1,28 @@
+package lesson9Data;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileCreator {
+
+    private static final String PATH = "C:/Users/Kolorek/IdeaProjects/Pat_Jel_Maven_Java_project/src/main/java/lesson9Data/data.txt";
+
+    public static void main(String[] args) throws IOException {
+
+        String[] lines = {"Kurs", "Java", "Lekcja 6", "Pliki", "Wyjątki", "Pliki", "Koniec pliku"};
+
+        File file = new File(PATH);
+        file.createNewFile();
+
+        BufferedWriter stream = Files.newBufferedWriter(Path.of(PATH));
+
+        for (String line : lines){
+            stream.newLine();
+            stream.write(line);
+        }
+        stream.flush();
+    }
+}
