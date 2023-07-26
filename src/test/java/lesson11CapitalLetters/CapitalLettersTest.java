07e83a0dcc267220691bfce7b988a76c2fdf2344
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-27 00:14:11 +0200

    created method for capitalizing letters and test

--- src/test/java/lesson11CapitalLetters/CapitalLettersTest.java
+++ src/test/java/lesson11CapitalLetters/CapitalLettersTest.java
@@ -1,13 +1,10 @@
 package lesson11CapitalLetters;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CapitalLettersTest {
 
