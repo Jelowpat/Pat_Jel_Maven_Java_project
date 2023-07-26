e66cefad4c124473ca2954fd11ddfb5749331b6a
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-26 22:36:57 +0200

    added modulo 3 checker with Exception handler

---
+++ src/main/java/lesson10Modulo3/Modulo3.java
@@ -0,0 +1,18 @@
+package lesson10Modulo3;
+
+public class Modulo3 {
+
+    public void validate(int[] values) throws IllegalArgumentException{
+        for (int element : values){
+            try {
+                if (element % 3 != 0){
+                    throw new IllegalArgumentException("Not divisible by 3");
+                }
+                System.out.println(element + " is divisible by 3");
+            } catch (IllegalArgumentException exception){
+                System.out.println(exception);
+            }
+        }
+    }
+
+}
