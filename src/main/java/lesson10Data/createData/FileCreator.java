83800a6196447162790e1ef33237016c3a1c5392
Jelowpat <patryk.jelowicki@gmail.com>
2023-07-29 11:38:14 +0200

    refactored the code

--- src/main/java/lesson10Data/createData/FileCreator.java
+++ src/main/java/lesson10Data/createData/FileCreator.java
@@ -3,26 +3,18 @@ package lesson10Data.createData;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Arrays;
 
 public class FileCreator {
 
-    private static final String PATH = "C:/Users/Kolorek/IdeaProjects/Pat_Jel_Maven_Java_project/src/main/java/lesson9Data/data.txt";
+    private static final Path PATH = Paths.get("src/main/java/lesson10Data/data.txt");
 
     public static void main(String[] args) throws IOException {
 
         String[] lines = {"Kurs", "Java", "Lekcja 6", "Pliki", "Wyjątki", "Pliki", "Koniec pliku"};
 
-        File file = new File(PATH);
-        file.createNewFile();
+        Files.write(PATH, Arrays.asList(lines), StandardOpenOption.CREATE);
 
-        BufferedWriter stream = Files.newBufferedWriter(Path.of(PATH));
-
-        for (String line : lines){
-            stream.newLine();
-            stream.write(line);
-        }
-        stream.flush();
     }
 }
