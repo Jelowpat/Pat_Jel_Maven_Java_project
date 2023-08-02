258f3ebf58d7e1e5c5ca3dad9a8dceea0830a9f6
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-02 22:42:58 +0200

    added message to exception

--- src/main/java/lesson11CapitalLetters/CapitalLetters.java
+++ src/main/java/lesson11CapitalLetters/CapitalLetters.java
@@ -4,7 +4,7 @@ public class CapitalLetters {
 
     public static String capitalize(String text){
         if (text == null){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("provided argument should not be null");
         }
             return text.toUpperCase();
     }
