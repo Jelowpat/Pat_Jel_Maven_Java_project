25711ee7c8932072fa3e60ef1db32cda8bb55c66
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-02 21:50:22 +0200

    changed the way of iterating on the list

--- src/main/java/lesson10Data/ReverseReader.java
+++ src/main/java/lesson10Data/ReverseReader.java
@@ -3,20 +3,20 @@ package lesson10Data;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 
 public class ReverseReader {
-    private static final Path filePath = Path.of
-            ("src/main/java/lesson10Data/data.txt");
+    private static final Path filePath = Path.of("src/main/java/lesson10Data/data.txt");
+
+    public static void main(String[] args) throws IOException {
+        new ReverseReader().readReversed();
+    }
 
     public void readReversed() throws IOException {
 
         var lines = Files.readAllLines(filePath);
 
-        Collections.reverse(lines);
-
-        for (String line : lines){
-            System.out.println(line);
+        for (int i = lines.size(); i--> 0;){
+            System.out.println(lines.get(i));
         }
 
     }
