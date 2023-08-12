d74732f0ef112731f2312f5489513d56e9ada761
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-12 13:09:52 +0200

    created Url validator using regex and tests

---
+++ src/test/java/lesson14UrlValidator/UrlValidatorTest.java
@@ -0,0 +1,41 @@
+package lesson14UrlValidator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlValidatorTest {
+
+    @ParameterizedTest(name = "validating url - {0}, should be {1}")
+    @MethodSource("provideUrlForValidation")
+    public void shouldValidate(String url, boolean expected) {
+        //given
+        UrlValidator urlValidator = new UrlValidator();
+
+        //when
+        boolean result = urlValidator.validate(url);
+
+        //then
+        Assertions.assertEquals(expected, result);
+
+
+    }
+
+    private static Stream<Arguments> provideUrlForValidation(){
+        return Stream.of(
+                Arguments.of("http://www.onet.pl","true"),
+                Arguments.of("https://mail.google.com","true"),
+                Arguments.of("http://onet.pl","true"),
+                Arguments.of("http://wiadmosci.onet.pl","true"),
+                Arguments.of("itsnoturl", "false")
+        );
+    }
+
+}
