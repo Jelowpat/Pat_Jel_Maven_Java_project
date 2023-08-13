d6adbf27849c649a15d39cab9b958d0483f0c696
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-14 01:22:07 +0200

    implemented the bubble sort based on hashcode

--- src/main/java/lesson13BubbleSort/BubbleSorter.java
+++ src/main/java/lesson13BubbleSort/BubbleSorter.java
@@ -3,13 +3,13 @@ import java.util.List;
 
 public class BubbleSorter {
 
-    public static void sort(List<Integer> list){
+    public static <E> void sort(List<E> list){
         int listLength = list.size();
         int iterations = list.size();
         do {
             for (int i = 0; i < listLength-1; i++){
-                if (list.get(i) > list.get(i + 1)){
-                    int first = list.get(i);
+                if (list.get(i).hashCode() > list.get(i + 1).hashCode()){
+                    E first = list.get(i);
                     list.set(i, list.get(i+1));
                     list.set(i+1, first);
                 }
