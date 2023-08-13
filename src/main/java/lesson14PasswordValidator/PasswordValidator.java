1d30c07fe48e558685d6d51d38887cb5ffa55ac7
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-13 22:29:25 +0200

    changed password validator to one regex

--- src/main/java/lesson14PasswordValidator/PasswordValidator.java
+++ src/main/java/lesson14PasswordValidator/PasswordValidator.java
@@ -1,16 +1,15 @@
 package lesson14PasswordValidator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
 
+    private static final Pattern PATTERN = Pattern.compile("^(?=(.*[A-Z]))(?=(.*[0-9]))(?!(.*(2021|21))).{7,}$");
+
     public boolean validate(String password){
-        Pattern patternCapitalLetter = Pattern.compile("[A-Z]");
-        Pattern patternDigit = Pattern.compile("\\d");
-        Pattern patternLength = Pattern.compile(".{7,}");
-        Pattern patternYear = Pattern.compile("(21|2021)");
-        return patternCapitalLetter.matcher(password).find() && patternDigit.matcher(password).find() &&
-                patternLength.matcher(password).matches() && !patternYear.matcher(password).find();
+        Matcher matcher = PATTERN.matcher(password);
+        return matcher.find();
     }
 
 }
