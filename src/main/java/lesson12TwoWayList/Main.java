ed6bb4187b947d5f13210ac86bc8be36e8bd914a
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-18 01:42:44 +0200

    all done...

--- src/main/java/lesson12TwoWayList/Main.java
+++ src/main/java/lesson12TwoWayList/Main.java
@@ -3,15 +3,42 @@ package lesson12TwoWayList;
 public class Main {
 
     public static void main(String[] args) {
-        MyTwoWayList myTwoWayList = new MyTwoWayList();
-        myTwoWayList.add("value1");
-        System.out.println(myTwoWayList);
-        myTwoWayList.add("value2");
-        System.out.println(myTwoWayList);
-        myTwoWayList.add("value3");
-        System.out.println(myTwoWayList);
-        myTwoWayList.add("value4");
-        System.out.println(myTwoWayList);
+        MyTwoWayList mylist = new MyTwoWayList();
+        mylist.add("1");
+        mylist.add("2");
+        mylist.add("3");
+        mylist.add("4");
+        mylist.add("5");
+        mylist.add("6");
+        mylist.add("7");
+        mylist.remove(4);
+        System.out.println(mylist); //displays list twice, to check if last and first nodes are connected properly
+        mylist.printReversed();  //displays list twice, to check if last and first nodes are connected properly
+        mylist.remove(0);
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.remove(4);
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add(0,"01");
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add(3,"23");
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add(3,"23");
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add("8");
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add("9");
+        System.out.println(mylist);
+        mylist.printReversed();
+        mylist.add("10");
+        System.out.println(mylist);
+        mylist.printReversed();
+
     }
 
 }
--- src/main/java/lesson12TwoWayList/MyTwoWayList.java
+++ src/main/java/lesson12TwoWayList/MyTwoWayList.java
@@ -1,56 +1,144 @@
 package lesson12TwoWayList;
 
+import java.util.NoSuchElementException;
+
 public class MyTwoWayList {
 
     private Node firstNode;
+    private Node lastNode;
+    private int size = 0;
+
 
     public void add(String value){
         if (firstNode == null){
-            firstNode = new Node(value);
+            lastNode = firstNode = new Node(value);
         }else{
-            Node temporaryNode = firstNode;
-            while(temporaryNode.next != null){
-                temporaryNode = temporaryNode.next;
-            }
-            temporaryNode.next = new Node(value);
+            Node temporaryLastNode = lastNode;
+            lastNode = new Node(value, firstNode, temporaryLastNode);
+            temporaryLastNode.setNext(lastNode);
+            firstNode.setPrevious(lastNode);
         }
+        size++;
     }
 
     public void add(Integer index, String value){
+
+        if(index > size || index < 0){
+            throw new NoSuchElementException("index out of range");
+        }
+
+        Node temporaryNode = firstNode;
+        if(index == 0){
+            Node temporaryFirstNode = firstNode;
+            firstNode = new Node(value, temporaryNode, lastNode);
+            lastNode.setNext(firstNode);
+            temporaryFirstNode.setPrevious(firstNode);
+            size++;
+        }else if(index == size){
+            add(value);
+        }else {
+            for (int i = 0; i <= index; i++){
+                if(i == index){
+                    Node temporaryPreviousNode = temporaryNode.getPrevious();
+                    Node temporaryNextNode = temporaryNode;
+                    temporaryNode = new Node(value, temporaryNextNode, temporaryPreviousNode);
+                    temporaryPreviousNode.setNext(temporaryNode);
+                    temporaryNextNode.setPrevious(temporaryNode);
+                    break;
+                }
+                temporaryNode = temporaryNode.getNext();
+            }
+            size++;
+        }
+    }
+
+    public void remove(Integer index){
+        if(index >= size || index < 0){
+            throw new NoSuchElementException("index out of range");
+        }
+
         Node temporaryNode = firstNode;
         for (int i = 0; i <= index; i++){
             if(i == index){
-                temporaryNode.next = new Node(value, temporaryNode.next);
+                temporaryNode.getNext().setPrevious(temporaryNode.getPrevious());
+                temporaryNode.getPrevious().setNext(temporaryNode.getNext());
+                if(index == 0){
+                    firstNode = temporaryNode.getNext();
+                }
+                if(index + 1 == size){
+                    lastNode = firstNode.getPrevious();
+                }
             }
-            temporaryNode = temporaryNode.next;
+            temporaryNode = temporaryNode.getNext();
         }
+        size--;
     }
 
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         Node temporaryNode = firstNode;
-        while(temporaryNode != null) {
+        for(int i = 0; i < size*2; i++) {
             stringBuilder.append(temporaryNode.value).append(", ");
             temporaryNode = temporaryNode.next;
             }
         return stringBuilder.toString();
+    }
+
+    public void printReversed(){
+        StringBuilder stringBuilder = new StringBuilder();
+        Node temporaryNode = lastNode;
+        for(int i = 0; i < size*2; i++) {
+            stringBuilder.append(temporaryNode.value).append(", ");
+            temporaryNode = temporaryNode.previous;
         }
+        System.out.println(stringBuilder);
+    }
 
 
-    private class Node {
+    private static class Node {
         private final String value;
         private Node next;
+        private Node previous;
 
 
         Node(String value) {
             this.value = value;
+            this.next = this;
+            this.previous = this;
         }
 
         Node(String value, Node next) {
-            this.value = value;
+            this(value);
             this.next = next;
         }
 
+        Node(String value, Node next, Node previous) {
+            this(value, next);
+            this.previous = previous;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value='" + value + '\'' +
+                    '}';
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
     }
 }
