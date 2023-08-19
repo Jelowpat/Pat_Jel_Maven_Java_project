da1308678834179e747290c5e56fb18c35b3ddb5
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-19 23:19:54 +0200

    changed tests

--- src/test/java/lesson13BubbleSort/BubbleSorterTest.java
+++ src/test/java/lesson13BubbleSort/BubbleSorterTest.java
@@ -1,18 +1,20 @@
 package lesson13BubbleSort;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Stream;
 
 class BubbleSorterTest {
 
-    @Test
-    void shouldSort() {
-        //given
-        List<Integer> list = Arrays.asList(7,1,0,13,122,15);
+    @ParameterizedTest
+    @MethodSource("provideListsForSort")
+    void shouldSort(List<Integer> list) {
         //when
         BubbleSorter.sort(list);
         ListIterator<Integer> iterator = list.listIterator();
@@ -31,4 +33,13 @@ class BubbleSorterTest {
         //then
         Assertions.assertTrue(result);
     }
+
+    private static Stream<Arguments> provideListsForSort(){
+        return Stream.of(
+                Arguments.of(Arrays.asList(9,23,14124,1313,21212)),
+                Arguments.of(Arrays.asList(3131,2,3,1,31231,322)),
+                Arguments.of(Arrays.asList(634,16,345,25,64332,25))
+        );
+    }
+
 }
