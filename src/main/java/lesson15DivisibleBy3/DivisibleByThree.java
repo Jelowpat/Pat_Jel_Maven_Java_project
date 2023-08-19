b5df787168dd1a08e1132a7ec6884a15fd753945
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-20 00:24:32 +0200

    created divisible by 3

--- src/main/java/lesson15DivisibleBy3/DivisibleByThree.java
+++ src/main/java/lesson15DivisibleBy3/DivisibleByThree.java
@@ -5,12 +5,11 @@ import java.util.stream.IntStream;
 public class DivisibleByThree {
 
     public static void main(String[] args) {
-        printDivisibleFromRange(1,21);
-        printDivisibleFromRange(-11,22);
+        printDivisibleFromRange(3,21);
     }
 
-    private static void printDivisibleFromRange(int start, int finish){
-        IntStream.range(start, finish+1).filter(x -> x%3 == 0).forEach(System.out::println);
+    private static void printDivisibleFromRange(int first, int last){
+        IntStream.range(first, last+1).filter(x -> x % 3 == 0).forEach(System.out::println);
     }
 
 }
