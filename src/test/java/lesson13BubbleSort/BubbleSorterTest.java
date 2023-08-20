d7509036f15b0d44c5278382c328bfb74ec716c5
Jelowpat <patryk.jelowicki@gmail.com>
2023-08-20 02:11:12 +0200

    changed test

--- src/test/java/lesson13BubbleSort/BubbleSorterTest.java
+++ src/test/java/lesson13BubbleSort/BubbleSorterTest.java
@@ -7,38 +7,24 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Stream;
 
 class BubbleSorterTest {
 
     @ParameterizedTest
     @MethodSource("provideListsForSort")
-    void shouldSort(List<Integer> list) {
+    void shouldSort(List<Integer> list, List<Integer> sortedList) {
         //when
         BubbleSorter.sort(list);
-        ListIterator<Integer> iterator = list.listIterator();
-        boolean result = true;
-        while(true){
-            int v1 = iterator.next();
-            if(!iterator.hasNext()){
-                break;
-            }
-            int v2 = iterator.next();
-            if(v1 > v2) {
-                result = false;
-            }
-            iterator.previous();
-        }
         //then
-        Assertions.assertTrue(result);
+        Assertions.assertEquals(sortedList, list);
     }
 
     private static Stream<Arguments> provideListsForSort(){
         return Stream.of(
-                Arguments.of(Arrays.asList(9,23,14124,1313,21212)),
-                Arguments.of(Arrays.asList(3131,2,3,1,31231,322)),
-                Arguments.of(Arrays.asList(634,16,345,25,64332,25))
+                Arguments.of(Arrays.asList(9,23,14124,1313,21212), Arrays.asList(9,23,1313,14124,21212)),
+                Arguments.of(Arrays.asList(3131,2,3,1,31231,322), Arrays.asList(1,2,3,322,3131,31231)),
+                Arguments.of(Arrays.asList(634,16,345,25,64332,25), Arrays.asList(16,25,25,345,634,64332))
         );
     }
 
